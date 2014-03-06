4c0910dee294af0a826912f60ece80ddb82aa9df
adband <androadb@gmail.com>
2014-03-06 12:51:56 +0200

    dfd

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-     //d  sad
+     //d  sad          asd
 
 public class Test {
     public static void main(String[] args) throws IOException {
